4ad3df7a4692cda7563318aa7e7eb9b0249b8f3a
Devon <devon.mobley@gmail.com>
2021-09-02 19:21:32 -0500

    create-react-app init

--- jquery/index.js
+++ jquery/index.js
@@ -2,3 +2,5 @@ $('#add-grocery-item-button').click((e) => {
   const inputValue = $('#grocery-item-input').val();
   $('#grocery-list').append(`<li>${inputValue}</li>`);
 });
+
+// send https://www.youtube.com/watch?v=nYkdrAPrdcw
